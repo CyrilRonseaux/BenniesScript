2b367eab152a4e4dd7750a8a6d59baf04cd299db
gronyon <gronyon@gmail.com>
2019-08-18 16:42:49 +0200

    Initial commit. - Deal bennies to player - Reset back to a given level - Support for multi-benny types in a single deck - List players to deal bennies to - Documentation

---
+++ Bennies.js
@@ -0,0 +1,292 @@
+// Deal and give bennies to players
+var BenniesScript = (function() 
+{
+	'use strict';
+	
+	function registerEventHandlers() 
+	{
+		apicmd.on(
+			'bennies-deal', 
+			'Deal bennies to a player', 
+			'[--quantity QTY --card BENNYNAME] --deck BENNYDECK --player PLAYERNAME',
+			[
+				['-d', '--deck TEXT', 'Name of the deck where bennies to deal are stored.'],
+				['-c', '--card TEXT', 'Name of the card to deal as benny from the deck. If not given, will pick the first card in the deck. Perfect for bennies deck with a single card.'],
+				['-p', '--player TEXT', 'Name of the player to deal bennies to.'],
+				['-q', '--quantity TEXT', 'Number of bennies to deal to. Must be 1 or higher. Defaults to 1.']
+			],
+			handleDealBennies
+		);
+		
+		apicmd.on(
+			'bennies-reset', 
+			'Reset a players bennie back to a given session start stock.', 
+			'[--quantity QTY --card BENNYNAME] --deck BENNYDECK --player PLAYERNAME',
+			[
+				['-d', '--deck TEXT', 'Name of the deck where bennies to deal are stored'],
+				['-c', '--card TEXT', 'Name of the card to deal as benny from the deck. If not given, will pick the first card in the deck. Perfect for bennies deck with a single card.'],
+				['-p', '--player TEXT', 'Name of the player to deal bennies to'],
+				['-q', '--quantity TEXT', 'Number of bennies to deal to. Must be 0 or higher. 0 will remove all bennies. Defaults to 3.']
+			],
+			handleResetBennies
+		);
+	}
+	
+	function handleDealBennies(argv, msg) 
+	{
+		if (!argv || !argv.opts || !argv.opts.deck) { //  || !argv.opts.player
+			sendChat("api", "/w gm --deck and --player options are mandatory.");
+			return;
+		}
+		
+		// if player was not provided, present one button per player and let GM.
+		if (!argv.opts.player) {
+			var online = _getOnlinePlayers();
+			var buttons = "/w gm To whom do you want to deal bennies to ? <br/>";
+			online.forEach(function(p) {
+				var cardArg = "";
+				if (argv.opts.card) {
+					cardArg = " --card &quot;" + argv.opts.card + "&quot;";
+				}
+				var quantityArg = "";
+				if (argv.opts.quantity) {
+					quantityArg = " --quantity " + argv.opts.quantity;
+				}
+				buttons = buttons + "[" + p.get("displayname") + "](!bennies-deal --player &quot;" + p.get("displayname") + "&quot; --deck &quot;" + argv.opts.deck + "&quot;" + cardArg + quantityArg + ") ";
+			});
+			
+			sendChat("api", buttons);
+			return;
+		}
+		
+		var player = _getPlayerByName(argv.opts.player);
+		if (!player) {
+			sendChat("api", "/w gm Player does not exist " + argv.opts.player + ".");
+			return;
+		}
+		
+		var playerHand = _getPlayerHand(player.get("id"));
+		if (!playerHand) {
+			sendChat("api", "/w gm Player hand does not exist " + player.get("name") + " (See API forums, this should never happen).");
+			return;
+		}	
+		
+		var benniesDeck = _getDeckByName(argv.opts.deck);
+		if (!benniesDeck) {
+			sendChat("api", "/w gm Deck not found " + argv.opts.deck);
+			return;
+		}
+		
+		var bennyCard = null;
+		if (argv.opts.card) {
+			bennyCard = _getCardInDeck(argv.opts.card, benniesDeck.get('id'));
+			if (!bennyCard) {
+				sendChat("api", "/w gm Card " + argv.opts.card + " not found in deck " + argv.opts.deck);
+				return;
+			}
+		} else {
+			bennyCard = _getFirstCardOfDeck(benniesDeck.get('id'));
+			if (!bennyCard) {
+				sendChat("api", "/w gm No card in deck " + argv.opts.deck);
+				return;
+			}
+		}
+		
+		var quantity = 1;
+		// if quantity not provided or invalid, assume dealing a single benny
+		if (argv.opts.quantity) {
+			var q = parseInt(argv.opts.quantity);
+			if (Number.isNaN(q)) {
+				sendChat("api", "/w gm Quantity " + argv.opts.quantity + " is not a valid number.");
+				return;
+			}
+			if (q == 0) {
+				sendChat("api", "/w gm You shall deal at least one (instead of " + argv.opts.quantity + ").");
+				return;
+			}
+			quantity = q;
+		}
+		
+		var bennyCardId = bennyCard.get("_id");
+		var handAsArray = playerHand.get("currentHand").split(",");
+		var i;
+		for (i = 0; i < quantity; i++) { 
+			handAsArray.push(bennyCardId);
+		}
+		playerHand.set("currentHand", handAsArray.join(","));
+		_niceChat(bennyCard.get("avatar"), "Dealt " + quantity + " " + bennyCard.get("name") + " to **" + player.get("displayname") + "**.");
+	}
+	
+	function handleResetBennies(argv, msg) 
+	{
+		if (!argv || !argv.opts || !argv.opts.deck || !argv.opts.player) {
+			sendChat("api", "/w gm --deck and --player options are mandatory.");
+			return;
+		}
+		
+		var player = _getPlayerByName(argv.opts.player);
+		if (!player) {
+			sendChat("api", "/w gm Player does not exist " + argv.opts.player + ".");
+			return;
+		}
+		
+		var playerHand = _getPlayerHand(player.get("id"));
+		if (!playerHand) {
+			sendChat("api", "/w gm Player hand does not exist " + player.get("name") + " (See API forums, this should never happen).");
+			return;
+		}	
+		
+		var benniesDeck = _getDeckByName(argv.opts.deck);
+		if (!benniesDeck) {
+			sendChat("api", "/w gm Deck not found " + argv.opts.deck);
+			return;
+		}
+		
+		var bennyCard = null;
+		if (argv.opts.card) {
+			bennyCard = _getCardInDeck(argv.opts.card, benniesDeck.get('id'));
+			if (!bennyCard) {
+				sendChat("api", "/w gm Card " + argv.opts.card + " not found in deck " + argv.opts.deck);
+				return;
+			}
+		} else {
+			bennyCard = _getFirstCardOfDeck(benniesDeck.get('id'));
+			if (!bennyCard) {
+				sendChat("api", "/w gm No card in deck " + argv.opts.deck);
+				return;
+			}
+		}
+		
+		var quantity = 3;
+		// if quantity not provided, assume reseting back to 3 bennies
+		if (argv.opts.quantity) {
+			var q = parseInt(argv.opts.quantity);
+			if (Number.isNaN(q)) {
+				sendChat("api", "/w gm Quantity " + argv.opts.quantity + " is not a valid number.");
+				return;
+			}
+			quantity = q;
+		}
+		
+		var bennyCardId = bennyCard.get("_id");
+		var handAsArray = playerHand.get("currentHand").split(",");
+		
+		// First removing all cards (if any) we need to reset
+		handAsArray = handAsArray.filter(cardInHand => cardInHand != bennyCardId);
+		
+		// Then (re)add cards up to the reset quantity
+		if (quantity > 0) {
+			var i;
+			for (i = 0; i < quantity; i++) { 
+				handAsArray.push(bennyCardId);
+			}
+		}
+		
+		playerHand.set("currentHand", handAsArray.join(","));
+		if (quantity > 0) {
+			_niceChat(bennyCard.get("avatar"), "Reset **" + player.get("displayname") + "** " + bennyCard.get("name") + " back to " + quantity + ".");
+		} else {
+			_niceChat(bennyCard.get("avatar"), "Reset **" + player.get("displayname") + "** " + bennyCard.get("name") + " back to nothing.");
+		}
+	}
+	
+	function _getOnlinePlayers()
+	{
+		var players = findObjs({
+			_type: "player" // ,
+			// _online: true
+		});
+		
+		return players;
+	}
+	
+	function _getPlayerByName(name)
+	{
+		var players = findObjs({
+			_type: "player",
+			_displayname: name
+		});
+		
+		if (!players) {
+			return null;
+		}
+		return players[0]; // Who would name oneself just like another player hmm ?!
+	}
+	
+	function _getDeckByName(name) 
+	{
+		var decks = findObjs({
+			_type: "deck",
+			name: name
+		});
+		
+		if (!decks) {
+			return null;
+		}
+		return decks[0]; // You shall not name two decks the same. I'll just pick the first found.
+	}
+	
+	function _getCardInDeck(card, deckId) 
+	{
+		var cards = findObjs({
+			_type: "card",
+			name: card,
+			deckid: deckId
+		});
+	
+		if (!cards) {
+			return null;
+		}
+		return cards[0]; // let's assume if multiple cards with the same name, they are all the same
+	}
+
+	function _getFirstCardOfDeck(deckId) 
+	{
+		var cards = findObjs({
+			_type: "card",
+			deckid: deckId
+		});
+		
+		if (!cards) {
+			return null;
+		}
+		return cards[0];
+	}
+	
+	function _getPlayerHand(playerId) 
+	{
+		var hands = findObjs({
+			_type: "hand",
+			_parentid: playerId
+		});
+		
+		if (!hands) {
+			return null;
+		}
+		return hands[0]; // players shall never have more than one hand.
+	}
+	
+	function _niceChat(bennyImage, message)
+	{
+		var html = '/desc '
+		+ '<div style="display: block; margin-left: -7px; margin-right: 2px; padding: 2px 0px;">'
+		+ '  <div style="position: relative; border: 1px solid #000; border-radius: 5px; background-color:ForestGreen; background-image: linear-gradient(rgba(255, 255, 255, .3), rgba(255, 255, 255, 0)); margin-right: -2px; padding: 2px 5px 5px 50px;">'
+		+ '    <div style="position: absolute; top: -10px; left: 5px; height: 40px; width: 40px;"><img src="' + bennyImage + '" style="height: 40px; width: 40px;" /></div>'
+		+ '    <div style="font-family: Candal; font-size: 13px; line-height: 15px; color: #FFF; font-weight: normal; text-align: center;">' + message + '</div>'
+		+ '  </div>'
+		+ '</div>';
+		
+		sendChat("", html);
+	}
+	
+	return {
+		registerEventHandlers: registerEventHandlers,
+		handleDealBennies: handleDealBennies
+	}
+}());
+
+on("ready", function()
+{
+	BenniesScript.registerEventHandlers();
+});
+
--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # BenniesScript
 Roll20 script to ease dealing Savage Worlds Bennies to players.
+
+## Setup
+You need a Roll20 Pro Subscription to be able to use API Scripts.
+
+You will need to install two scripts :
+* apicmd.js that you can find ![apicmd GitHub](https://gist.github.com/goblinHordes/7424738)
+* this Bennies.js script from ![Gronyon's GitHub](https://github.com/gronyon/BenniesScript)
+apicmd is used to parse the BenniesScript input (why reinvent the wheel when it has already been created and nicely round ?).
+
+You will also need to create a Deck of "cards" for your bennies, in your Roll20 game.
+* Name of the deck : it will appear in your players hand of cards and will also be used in this script API commands. Something like "Bennies" should do.
+* Set the Deck to unlimited.
+* ![Benny deck setup](benny-deck.png)
+* Create one card, give it a name. This name can be used in the API (see below).
+* Uploade a visual for your card and the deck background. I personnaly use the same image.
+* ![Benny card setup](benny-card.png)
+
+
+## API Commands
+### Dealing bennies to a player
+This will give a single benny to the given player :
+```javascript
+!bennies-deal --player PlayerDisplayName --deck NameOfTheBennyDeck
+```
+![Dealt one benny](deal-1.png)
+![One benny in hand](gronyon-hand-1.png)
+
+If player display name contain spaces or special characters, use quotes (same for your bennies deck name if needed) :
+```javascript
+!bennies-deal --player "A player name" --deck "Wonderful Bennies of Awesome"
+```
+
+If you ever need to give more than one benny to a player :
+```javascript
+!bennies-deal --player PlayerDisplayName --deck NameOfTheBennyDeck --quantity 4
+```
+![Dealt four bennies](deal-4.png)
+![Four bennies in hand](gronyon-hand-4.png)
+
+
+And you can short it all :
+```javascript
+!bennies-deal -p PlayerDisplayName -d NameOfTheBennyDeck -q 2
+```
+
+If you don't provide a player name, the commande will list all players and let you pick one :
+```javascript
+!bennies-deal -d NameOfTheBennyDeck
+```
+![Player name omitted will list all players](omit-player-name.png)
+
+### Using bennies (players)
+No need to use script api here. Players will simply drag and drop their benny card on the table.
+![Drag and drop a benny](drag-and-drop.png)
+
+If that was a mistake, they can simply pick it back.
+![Pick the benny back](oops.png)
+
+If it wasn't, Game Master can simply delete the benny from the table.
+![Delete benny from table](pick-it.png)
+
+
+### Reseting bennies at the start of a game
+At the start of each session, you want to refill each player to its standard starting pool.
+
+You can use the reset command, which syntax looks exactly as the deal command, but it will reset to the given quantity instead of adding.
+```javascript
+!bennies-reset -p PlayerDisplayName -d NameOfTheBennyDeck
+```
+Bennies will be reset to 3.
+
+If player has Luck or similary Edge, you can set the quantity :
+```javascript
+!bennies-reset -p PlayerDisplayName -d NameOfTheBennyDeck -q 4
+```
+![Reseting bennies](benny-reset-4.png)
+
+## Setting up macros
+
+
+
+
+## Handling different type of bennies
+If your game is using different types of bennies (like in Deadlands), for example, a Classic benny that can reroll a trait, a Soak benny that can only soak wounds, and a royal benny that can be used for both, you can use a different deck for each type of benny, and change the Deck name in the command.
+```javascript
+!bennies-deal -p PlayerDisplayName -d Royal 
+```
+
+Those bennies will appear in separate sections (one per Deck) in the player's hand :
+![One deck per type of bennies](multi-bennies-as-multi-deck.png)
+
+Or you can put all bennies as different cards in the same unique deck. Each card must have a different name.
+![All bennies in same deck](multi-benny.png)
+
+You can then deal a specific benny to a player by adding a --card argument :
+```javascript
+!bennies-deal -p Dude -d Bennies --card Royal
+```
+
+Bennies will all appear in the same section :
+![Single benny section](single-benny-section.png)
+
+To reset bennies, you will have to specify each type of benny for each player :
+```javascript
+!bennies-reset -p Dude -d Bennies --card Classic --quantity 3
+!bennies-reset -p Dude -d Bennies --card Soak --quantity 1
+!bennies-reset -p Dude -d Bennies --card Royal --quantity 0
+```
+
